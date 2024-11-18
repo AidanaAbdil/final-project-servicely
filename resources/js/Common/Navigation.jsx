31fe2623d555733e486bbe2a0904153b709b18d2
adinazhan <a.zhantykeyeva@gmail.com>
2024-11-18 16:31:58 +0100

    registration functionality created

--- resources/js/Common/Navigation.jsx
+++ resources/js/Common/Navigation.jsx
@@ -1,15 +1,23 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
 import Searchbar from "../Components/Searchbar";
 
 function Navigation() {
     return (
         <nav>
-            <p>Logo</p>
+            <p>LOGO</p>
             <Searchbar />
             <a href="">Home</a>
             <a href="">About Us</a>
             <a href="">Catalog</a>
-            <a href="">Login</a>
-            <a href="">Register</a>
+
+            <Link to={`/register`}>
+                <button role="link">Register</button>
+            </Link>
+            <Link to={`/login`}>
+                <button role="link">Login</button>
+            </Link>
         </nav>
     );
 }
